84fafeb486b415dc73729d2cd84e2a4903a9d226
Masumi Hayashi-Smith <masumi.hs@gmail.com>
2015-12-06 22:30:40 -0800

    fix routing

--- app/App/App.js
+++ app/App/App.js
@@ -4,45 +4,28 @@ import React from 'react';
 import SongView from '../SongView/SongView.js';
 import CourseView from '../CourseView/CourseView.js';
 
+import { Link } from 'react-router';
+
 
 export default class App extends React.Component {
   // same thing as `getInitialState` in ES5
   constructor(props) {
     super(props);
-    this.state = {
-      songView: true
-    };
-  }
-
-  toggleCourses() {
-    console.log(this.state);
-    this.setState({songView: !this.state.songView});
+    this.state = {};
   }
 
   render() {
+    return (
+      <div>
+        <Link to={`/song`}>
+          Song View
+        </Link>
 
-    if (this.state.songView === true) {
-      return (
-        <div>
-          <button
-            onClick={this.toggleCourses.bind(this)}
-            className={styles.switchBtn}>
-            toggleCourses
-          </button>
-          <SongView {... this.props}/>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <button
-            onClick={this.toggleCourses.bind(this)}
-            className={styles.switchBtn}>
-            toggleCourses
-          </button>
-          <CourseView {... this.props}/>
-        </div>
-      );
-    }
+        <Link to={`/courses`}>
+          Course View
+        </Link>
+        {this.props.children}
+      </div>
+    );
   }
 }
--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 import React from 'react/addons.js';
 import App from './App/App.js';
+import SongView from './SongView/SongView.js';
 import './main.css';
 import { Router, Route, Link } from 'react-router';
 
@@ -7,7 +8,8 @@ import { Router, Route, Link } from 'react-router';
 React.render((
   <Router>
     <Route path="/" component={App}>
-      <Route path="/song/:songID" component={App}/>
+      <Route path="/song/:songID" component={SongView}/>
+      <Route path="*" component={SongView} />
     </Route>
   </Router>
 ), document.getElementById('app'));
